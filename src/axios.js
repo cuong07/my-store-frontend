1e1a8f1821246ddb1f3c56f68d4d3e131e0f7544
cuong <front.cuong@gmail.com>
2023-04-23 01:06:46 +0700

    full

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const request = axios.create({
     baseURL: process.env.REACT_APP_SEVER_URL,
     withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
 
 export default request;
